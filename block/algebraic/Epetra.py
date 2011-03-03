4854e495d552d814588c9bd6252fedc9d967595e
Joachim B Haga <jobh@simula.no>
2011-03-03 09:48:05 +0100

    Added explicit matrix multiply

--- block/algebraic/Epetra.py
+++ block/algebraic/Epetra.py
@@ -3,80 +3,126 @@ from __future__ import division
 from dolfin import Vector
 from block.blockbase import blockbase
 
-class LumpedJacobi(blockbase):
-    def __init__(self, A):
+class diag_op(blockbase):
+    def __init__(self, v):
         from PyTrilinos import Epetra
-
-        A = A.down_cast().mat()
-        self.diag = Epetra.Vector(A.RowMap())
-        A.InvRowSums(self.diag)
+        assert isinstance(v, (Epetra.MultiVector, Epetra.Vector))
+        self.v = v
 
     def matvec(self, b):
         if not isinstance(b, Vector):
             return NotImplemented
         x = Vector(len(b))
-        x.down_cast().vec().Multiply(1, self.diag, b.down_cast().vec(), 0)
-
+        x.down_cast().vec().Multiply(1, self.v, b.down_cast().vec(), 0)
         return x
 
-class MatMult(blockbase):
-    def __init__(self, A, B, scale=None):
-        from PyTrilinos import Epetra, EpetraExt
+    def matmat(self, other):
+        try:
+            from numpy import isscalar
+            from PyTrilinos import Epetra
+            if isscalar(other):
+                x = Epetra.Vector(self.v)
+                x.Scale(other)
+                return diag_op(other)
+            other = other.down_cast()
+            if hasattr(other, 'mat'):
+                C = Epetra.CrsMatrix(other.mat())
+                C.LeftScale(self.v)
+                return matrix_op(C)
+            else:
+                x = Epetra.Vector(other.vec().EpetraMap())
+                x.Multiply(1, self.v, other.vec(), 0)
+                return diag_op(x)
+        except AttributeError:
+            raise RuntimeError, "can't extract matrix data from type '%s'"%str(type(other))
 
-        A = A.down_cast().mat()
-        B = B.down_cast().mat()
+    def down_cast(self):
+        return self
+    def vec(self):
+        return self.v
 
-        if scale is not None:
-            B = Epetra.CrsMatrix(B)
-            B.LeftScale(scale)
+class matrix_op(blockbase):
+    def __init__(self, M):
+        from PyTrilinos import Epetra
+        assert isinstance(M, (Epetra.CrsMatrix, Epetra.FECrsMatrix))
+        self.M = M
 
-        C = Epetra.CrsMatrix(Epetra.Copy, A.RowMap(), 100)
-        assert (0 == EpetraExt.Multiply(A, False, B, False, C))
-        C.OptimizeStorage()
+    def matvec(self, b):
+        if not isinstance(b, Vector):
+            return NotImplemented
+        x = Vector(len(b))
+        self.M.Apply(b.down_cast().vec(), x.down_cast().vec())
+        return x
 
-        self.C = C
+    def matmat(self, other):
+        try:
+            from numpy import isscalar
+            from PyTrilinos import Epetra
+            if isscalar(other):
+                C = Epetra.CrsMatrix(self.M)
+                C.Scale(other)
+                return matrix_op(C)
+            other = other.down_cast()
+            if hasattr(other, 'mat'):
+                from PyTrilinos import EpetraExt
+                C = Epetra.CrsMatrix(Epetra.Copy, self.M.RowMap(), 100)
+                assert (0 == EpetraExt.Multiply(self.M, False, other.mat(), False, C))
+                C.OptimizeStorage()
+                return matrix_op(C)
+            else:
+                C = Epetra.CrsMatrix(self.M)
+                C.RightScale(other.vec())
+                return matrix_op(C)
+        except AttributeError:
+            raise RuntimeError, "can't extract matrix data from type '%s'"%str(type(other))
 
-    # For algebraic preconditioners --- they call A.down_cast().mat() to access Epetra matrix
     def down_cast(self):
         return self
     def mat(self):
-        return self.C
+        return self.M
 
-    def matvec(self, b):
-        if not isinstance(b, Vector):
-            return NotImplemented
-        x = Vector(len(b))
-        self.C.Apply(b.down_cast().vec(), x.down_cast().vec())
-        return x
-
-class SchurComplement(MatMult):
-    """Return the Schur complement of the (2,2) block. A diagonal approximation
-    is used for the inverse of the (1,1) block.
-
-    C * inv(diag(A)) * B - D"""
-    def __init__(self, AA):
+class Diag(diag_op):
+    def __init__(self, A):
         from PyTrilinos import Epetra
-        import numpy
-        from block.blockoperator import blockop
+        A = A.down_cast().mat()
+        v = Epetra.Vector(A.RowMap())
+        A.ExtractDiagonalCopy(v)
+        diag_op.__init__(self, v)
 
-        AA = blockop(AA)
-        if AA.blocks.shape != (2,2):
-            raise TypeError, "SchurComplement: expected 2x2 blocks"
+class InvDiag(Diag):
+    def __init__(self, A):
+        Diag.__init__(self, A)
+        self.v.Reciprocal(self.v)
 
-        A,B = AA[0,:]
-        C,D = AA[1,:]
+class LumpedInvDiag(diag_op):
+    def __init__(self, A):
+        from PyTrilinos import Epetra
+        A = A.down_cast().mat()
+        v = Epetra.Vector(A.RowMap())
+        A.InvRowSums(v)
+        diag_op.__init__(self, v)
 
-        Adiag = Epetra.Vector(A.down_cast().mat().RowMap())
-        A.down_cast().mat().ExtractDiagonalCopy(Adiag)
-        Adiag.Reciprocal(Adiag)
-
-        MatMult.__init__(self, C, B, scale=Adiag)
-
-        if numpy.isscalar(D):
-            if D != 0:
-                # Extract diagonal of S
-                # Add D to diagonal
-                # Put diagonal back
-                raise NotImplementedError, 'SchurComplement: The (2,2) block must be a matrix or scalar 0'
+class explicit(matrix_op):
+    def __init__(self, x):
+        from block.blockcompose import blockcompose
+        from numpy import isscalar
+        from dolfin import Matrix
+        if isinstance(x, blockcompose):
+            factors = x.chain
+            while len(factors) > 1:
+                A = factors.pop()
+                B = factors.pop()
+                if isinstance(A, Matrix):
+                    _A = A # postpone garbage collection
+                    A = matrix_op(A.down_cast().mat())
+                if isinstance(B, Matrix):
+                    _B = B # postpone garbage collection
+                    B = matrix_op(B.down_cast().mat())
+                if isscalar(A):
+                    C = B.matmat(A)
+                else:
+                    C = A.matmat(B)
+                factors.append(C)
+            matrix_op.__init__(self, factors[0].down_cast().mat())
         else:
-            assert (0 == EpetraExt.Add(D.down_cast().mat(), False, -1, self.C, 1))
+            raise NotImplementedError, "explicit for type '%s'"%str(type(x))
