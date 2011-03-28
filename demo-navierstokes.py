119868ee3c8881e6e979fae54bb7150e6b5be95f
Joachim B Haga <jobh@simula.no>
2011-03-28 15:07:20 +0200

    Add a too-complex and not-really-working-very-well Navier-Stokes demo

--- demo-navierstokes.py
+++ demo-navierstokes.py
@@ -1,32 +1,23 @@
-"""This demo solves the Stokes equations with block preconditioning.
+"""This demo solves the Navier-Stokes equations with block preconditioning.
 
-The original demo is found in demo/undocumented/stokes-taylor-hood/python.
+NOTE: Needs work. Must define a better precond, this one doesn't work for smaller mu.
 
-The algebraic system to be solved can be written as
-
-  BB^ AA [sigma u]^T = BB^ [b 0]^T,
-
-where AA is a 2x2 block system with zero in the (2,2) block
-
-       | A   B |
-  AA = |       |,
-       | C   0 |
-
-and BB^ approximates the inverse of the block operator
-
-       | A   0 |
-  BB = |       |.
-       | 0   L |
+It is a modification of the Stokes demo (demo-stokes.py).
 """
 
 # Since we use ML from Trilinos, we must import PyTrilinos before any dolfin
 # modules. This works around a bug with MPI initialisation/destruction order.
+# Furthermore, scipy (in LGMRES) seems to crash unless it's loaded before
+# PyTrilinos.
+import scipy
 import PyTrilinos
 
 from dolfin import *
 from block import *
 from block.iterative import *
 from block.algebraic.trilinos import *
+
+dolfin.set_log_level(15)
 
 # Load mesh and subdomains
 mesh = Mesh("dolfin-2.xml.gz")
@@ -53,8 +44,10 @@ v, u = TestFunction(V), TrialFunction(V)
 q, p = TestFunction(Q), TrialFunction(Q)
 
 f = Constant((0, 0))
+w = Constant((1, 0))
+mu = Constant(1e-2)
 
-A  = assemble(inner(grad(v), grad(u))*dx)
+A  = assemble(mu*inner(grad(v), grad(u))*dx + inner(dot(grad(u),w),v)*dx)
 B  = assemble(div(v)*p*dx)
 C  = assemble(div(u)*q*dx)
 I  = assemble(p*q*dx)
@@ -74,17 +67,16 @@ b  = block_vec([b0, 0])
 bcs = block_bc([[bc0, bc1], [bc2]])
 bcs.apply(AA, b)
 
-# Create preconditioners: An ML preconditioner for A, and the ML inverse of the
-# mass matrix for the (2,2) block.
-Ap = ML(A)
-Ip = ML(I)
+# Create preconditioners: An ILU preconditioner for A, and an ML inverse of the
+# Schur complement approximation for the (2,2) block.
+Ap = ILU(A)
+Dp = ML(explicit(C*InvDiag(A)*B))
 
-prec = block_mat([[Ap, 0],
-                  [0, Ip]])
+prec = block_mat([[Ap, B],
+                  [C, -Dp]]).scheme('sgs')
 
-# Create the block inverse, using the preconditioned Minimum Residual method
-# (suitable for symmetric indefinite problems).
-AAinv = MinRes(AA, precond=prec, tolerance=1e-10, maxiter=500, show=2)
+# Create the block inverse, using the LGMRES method (suitable for general problems).
+AAinv = LGMRES(AA, precond=prec, tolerance=1e-5, maxiter=50, show=2)
 
 # Compute solution
 u, p = AAinv * b
