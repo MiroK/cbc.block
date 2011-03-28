a7c71fe201c0c7ac19becd07d0c2fb39e74a14af
Joachim B Haga <jobh@simula.no>
2011-03-28 15:17:27 +0200

    Changes to demo-transform from KAM

--- demo-transform.py
+++ demo-transform.py
@@ -5,27 +5,37 @@ from block import *
 from block.iterative import ConjGrad
 
 mesh = UnitSquare(2,2)
+
 V    = FunctionSpace(mesh, "CG", 1)
 v, u = TestFunction(V), TrialFunction(V)
 
-tensor = block_mat([[1,-1],
-                    [0, 1]])
+tensor1 = block_mat([[1, 0],
+                     [0, 1]])
+tensor2 = block_mat([[1,-1],
+                     [0, 1]])
 
 print 'Block matrix multiplication: composed object and its result'
 I = simplify(block_mat([[1, 0],
                         [0, 1]]))
-B = block_transpose(tensor)*(tensor+I)
+B = block_transpose(tensor1)*(tensor1+I)
 print '\nI =',I
 print '\nB =',B
 print '\nBx=',inside_out(B)
 
-A = assemble(u*v*dx)
-K = kronecker(A,tensor)
+
+M = assemble(u*v*dx)
+A = assemble(dot(grad(u),grad(v))*dx)
+
+K = kronecker(A,tensor1)
+L = kronecker(M,tensor2)
+
+AA = K + L 
+
 
 print '\n================='
 print 'Block matrix multiplication: Kronecker product and its result'
-print '\nK =', K
-print '\nKx=', inside_out(K)
+print '\nK =', AA 
+print '\nKx=', inside_out(AA)
 
 print '\n================='
 print 'Block matrix multiplication: Inverse of the Kronecker product.'
@@ -33,7 +43,7 @@ print 'This can be formed by explicit inversion of the tensor and an'
 print 'iterative solver, either blockwise or on the full system.'
 
 import numpy
-C,D = K
-Di  = block_mat(numpy.linalg.inv(tensor.blocks))
-print '\nKinv1=', kronecker(Di,ConjGrad(A))
+C,D = AA
+Di  = block_mat(numpy.linalg.inv(tensor1.blocks))
+print '\nKinv1=', kronecker(Di,ConjGrad(M+A))
 print '\nKinv2=', Di*ConjGrad(C)
