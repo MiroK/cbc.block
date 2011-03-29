8b6f987bb0b5457e1d038aca51b80c2521c29743
Joachim B Haga <jobh@simula.no>
2011-03-29 10:57:04 +0200

    Clean up inits, throw better error when mixing block containers with Dolfin operators

--- block/__init__.py
+++ block/__init__.py
@@ -11,55 +11,62 @@ In addition, methods are injected into dolfin.Matrix / dolfin.Vector as
 needed. This should eventually be moved to Dolfin proper.
 """
 
-import dolfin
 from block_mat import block_mat
 from block_vec import block_vec
 from block_compose import block_compose, block_add, block_sub, block_transpose
 from block_transform import block_kronecker, block_simplify, block_collapse
 from block_bc import block_bc
 
-# To make stuff like L=C*B work when C and B are type dolfin.Matrix, we inject
-# methods into dolfin.Matrix
+def _init():
+    import dolfin
+    from object_pool import vec_pool
+    from block_base import block_container
 
-def _wrap(cls, name, wrap_to):
-    _old_method = getattr(cls, name)
-    def _new_method(self, other):
-        y = _old_method(self, other)
-        if y == NotImplemented:
-            y = wrap_to(self, other)
-        return y
-    setattr(cls, name, _new_method)
+    # To make stuff like L=C*B work when C and B are type dolfin.Matrix, we inject
+    # methods into dolfin.Matrix
 
-_wrap(dolfin.Matrix, '__mul__', block_compose)
-_wrap(dolfin.Matrix, '__add__', block_add)
-_wrap(dolfin.Matrix, '__sub__', block_sub)
-del _wrap
+    def check_type(obj1, obj2):
+        if isinstance(obj2, block_container):
+            raise TypeError, 'cannot apply dolfin operators on block containers:\n\t%s\nand\n\t%s'%(obj1,obj2)
+        return True
 
-dolfin.Matrix.__rmul__ = lambda self, other: block_compose(other, self)
-dolfin.Matrix.__radd__ = lambda self, other: block_add(other, self)
-#dolfin.Matrix.__rsub__ = lambda self, other: block_sub(other, self)
-dolfin.Matrix.__neg__  = lambda self       : block_compose(-1, self)
+    def wrap(cls, name, wrap_to):
+        _old_method = getattr(cls, name)
+        def _new_method(self, other):
+            check_type(self, other)
+            y = _old_method(self, other)
+            if y == NotImplemented:
+                y = wrap_to(self, other)
+            return y
+        setattr(cls, name, _new_method)
 
-# Inject a new transpmult() method that returns the result vector (instead of output parameter)
-def _wrap_transpmult():
-    _old_transpmult = dolfin.Matrix.transpmult
-    def _transpmult(self, x):
+    wrap(dolfin.Matrix, '__mul__', block_compose)
+    wrap(dolfin.Matrix, '__add__', block_add)
+    wrap(dolfin.Matrix, '__sub__', block_sub)
+
+    dolfin.Matrix.__rmul__ = lambda self, other: check_type(self, other) and block_compose(other, self)
+    dolfin.Matrix.__radd__ = lambda self, other: check_type(self, other) and block_add(other, self)
+    #dolfin.Matrix.__rsub__ = lambda self, other: check_type(self, other) and block_sub(other, self)
+    dolfin.Matrix.__neg__  = lambda self       : block_compose(-1, self)
+
+    # Inject a new transpmult() method that returns the result vector (instead of output parameter)
+    old_transpmult = dolfin.Matrix.transpmult
+    def transpmult(self, x):
+        check_type(self, x)
         y = dolfin.Vector()
-        _old_transpmult(self, x, y)
+        old_transpmult(self, x, y)
         return y
-    dolfin.Matrix.transpmult = _transpmult
-_wrap_transpmult()
-del _wrap_transpmult
+    dolfin.Matrix.transpmult = transpmult
 
-# Inject a create() method that returns the new vector (instead of resize() which uses out parameter)
-def _create_vec(self, dim=1):
-    vec = dolfin.Vector()
-    self.resize(vec, dim)
-    return vec
-from object_pool import vec_pool
-dolfin.GenericMatrix.create_vec = vec_pool(_create_vec)
-del _create_vec, vec_pool
+    # Inject a create() method that returns the new vector (instead of resize() which uses out parameter)
+    def create_vec(self, dim=1):
+        vec = dolfin.Vector()
+        self.resize(vec, dim)
+        return vec
+    dolfin.GenericMatrix.create_vec = vec_pool(create_vec)
 
-# For the Trilinos stuff, it's much nicer if down_cast is a method on the object
-dolfin.GenericMatrix.down_cast = dolfin.down_cast
-dolfin.GenericVector.down_cast = dolfin.down_cast
+    # For the Trilinos stuff, it's much nicer if down_cast is a method on the object
+    dolfin.GenericMatrix.down_cast = dolfin.down_cast
+    dolfin.GenericVector.down_cast = dolfin.down_cast
+
+_init()
---
+++ block/iterative/iterative.py
@@ -0,0 +1,104 @@
+from __future__ import division
+"""Base class for iterative solvers."""
+
+from block.block_base import block_base
+
+class iterative(block_base):
+    def __init__(self, A, precond=1.0, tolerance=1e-5, initial_guess=None, iter=None, maxiter=200,
+                 name=None, show=1, **kwargs):
+        self.B = precond
+        self.A = A
+        self.initial_guess = initial_guess
+        self.show = show
+        self.kwargs = kwargs
+        self.name = name if name else self.__class__.__name__
+        if iter is not None:
+            tolerance = 0
+            maxiter = iter
+        self.tolerance = tolerance
+        self.maxiter = maxiter
+
+    def matvec(self, b):
+        from time import time
+        from block.block_vec import block_vec
+        from dolfin import info, Progress
+        TRACE = 13 # dolfin.TRACE
+
+        T = time()
+
+        # If x and initial_guess are block_vecs, some of the blocks may be
+        # scalars (although block_bc.apply() converts these to vectors, so
+        # normally they are not). To be sure, call allocate() on them.
+
+        if isinstance(b, block_vec):
+            b.allocate(self.A)
+
+        if self.initial_guess:
+            x = self.initial_guess
+            if isinstance(x, block_vec):
+                x.allocate(self.A)
+        else:
+            x = self.A.create_vec()
+            x.zero()
+
+        try:
+            info(TRACE, self.__class__.__name__+' solve of '+str(self.A))
+            if self.B != 1.0:
+                info(TRACE, 'Using preconditioner: '+str(self.B))
+            progress = Progress(self.name, self.maxiter)
+            x = self.method(self.B, self.A, x, b, tolerance=self.tolerance, maxiter=self.maxiter,
+                            progress=progress, **self.kwargs)
+            del progress # trigger final printout
+        except Exception, e:
+            from dolfin import warning
+            warning("Error solving " + self.name)
+            raise
+        x, self.residuals, self.alphas, self.betas = x
+
+        if self.tolerance == 0:
+            msg = "done"
+        elif self.converged:
+            msg = "converged"
+        else:
+            msg = "NOT CONV."
+
+        if self.show == 1:
+            info('%s %s [iter=%2d, time=%.2fs, res=%.1e]' \
+                % (self.name, msg, self.iterations, time()-T, self.residuals[-1]))
+        elif self.show == 2:
+            info('%s %s [iter=%2d, time=%.2fs, res=%.1e, true res=%.1e]' \
+                % (self.name, msg, self.iterations, time()-T, self.residuals[-1], (self.A*x-b).norm('l2')))
+        return x
+
+    @property
+    def iterations(self):
+        return len(self.residuals)-1
+    @property
+    def converged(self):
+        return self.tolerance == 0 or self.residuals[-1] < self.tolerance
+
+    def eigenvalue_estimates(self):
+        #####
+        # Adapted from code supplied by KAM (Simula PyCC; GPL license),
+        #####
+
+        # eigenvalues estimates in terms of alphas and betas
+
+        import numpy
+
+        n = len(self.alphas)
+        if n == 0:
+            raise RuntimeError, 'Eigenvalues can not be estimated, no alphas/betas'
+        M = numpy.zeros([n,n])
+        M[0,0] = 1/self.alphas[0]
+        for k in range(1, n):
+            M[k,k] = 1/self.alphas[k] + self.betas[k-1]/self.alphas[k-1]
+            M[k,k-1] = numpy.sqrt(self.betas[k-1])/self.alphas[k-1]
+            M[k-1,k] = M[k,k-1]
+        e,v = numpy.linalg.eig(M)
+        e.sort()
+        return e
+
+    def __str__(self):
+        return '<%d %s iterations on %s>'%(self.maxiter, self.name, self.A)
+
