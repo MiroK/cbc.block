af71618521e09cc3c9fb10cbfff8f428540440d6
Kent-Andre Mardal <kent-and@bigblue>
2011-04-16 19:54:17 +0200

    bug fix

--- book-chapter/run.py
+++ book-chapter/run.py
@@ -1,5 +1,5 @@
 
-import pjobs
+from dolfin_utils import pjobs
 
 
 jobs = ["sh poisson_neumann.sh > poisson_neumann.output", "sh stokes.sh > stokes.output", "sh timestokes.sh > timestokes.output", "sh hodge.sh > hodge.output" ] 
