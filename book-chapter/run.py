74a78414aa6b419b628d46672de1ff504f35784c
Kent-Andre Mardal <kent-and@bigblue>
2011-04-16 19:49:06 +0200

    added a script to run all jobs using pjobs

---
+++ book-chapter/run.py
@@ -0,0 +1,7 @@
+
+import pjobs
+
+
+jobs = ["sh poisson_neumann.sh > poisson_neumann.output", "sh stokes.sh > stokes.output", "sh timestokes.sh > timestokes.output", "sh hodge.sh > hodge.output" ] 
+
+pjobs.submit(jobs)
