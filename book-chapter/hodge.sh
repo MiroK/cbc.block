87e9ac36fcd86ab422bceb88269a276a99dc28ee
Kent-Andre Mardal <kent-and@simula.no>
2011-04-26 13:12:12 +0200

    moved files around

--- book-chapter/hodge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for N in 2 4 8 16 32; do
-    python hodge.py N=$N
-done
--- book-chapter/run.py
+++
@@ -1,7 +0,0 @@
-
-from dolfin_utils import pjobs
-
-
-jobs = ["sh poisson_neumann.sh > poisson_neumann.output", "sh stokes.sh > stokes.output", "sh timestokes.sh > timestokes.output", "sh hodge.sh > hodge.output" ] 
-
-pjobs.submit(jobs)
