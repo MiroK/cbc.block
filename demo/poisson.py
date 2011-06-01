fff1e23f044da2fbe827eb82ef2e011d4f44f39a
Kent-Andre Mardal <kent-and@simula.no>
2011-06-01 14:26:28 +0200

    added poisson test (not working)

---
+++ demo/poisson.py
@@ -0,0 +1,39 @@
+import PyTrilinos
+from block import *
+from block.iterative import *
+from block.algebraic.trilinos import *
+from dolfin import *
+from block.dolfin_util import *
+import numpy
+
+# Function spaces, elements
+
+mesh = UnitSquare(16,16)
+
+V = FunctionSpace(mesh, "CG", 1)
+
+f = Expression("sin(3.14*x[0])")
+u, v =  TrialFunction(V), TestFunction(V)
+
+a = u*v*dx + dot(grad(u), grad(v))*dx  
+L = f*v*dx 
+
+A = assemble(a)
+b = assemble(L)
+
+B = ML(A)
+
+Ainv = ConjGrad(A, precond=B, tolerance=1e-10, show=2) 
+
+x = A*b 
+
+u = Function(V)
+u.vector()[:] = x[:]
+plot(u, title="u")
+
+u2 = Function(V)
+solve(A, u2.vector(), b)
+plot(u2, title="u2")
+
+interactive()
+
