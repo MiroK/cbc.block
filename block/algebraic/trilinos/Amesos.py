b5af6f81461001016b19febab07a61d55bd44b45
Joachim Berdal Haga <jobh@simula.no>
2013-03-08 10:32:27 +0100

    Fix segfault in Amesos solver (and avoid some copying)

--- block/algebraic/trilinos/Amesos.py
+++ block/algebraic/trilinos/Amesos.py
@@ -13,29 +13,39 @@ class AmesosSolver(block_base):
     def __init__(self, A, solver='Klu'):
         from PyTrilinos import Epetra, Amesos
         self.A = A # Keep reference
-        self.b = A.create_vec(dim=0)
-        self.x = A.create_vec(dim=1)
-        problem = Epetra.LinearProblem(A.down_cast().mat(),
-                                       self.x.down_cast().vec(),
-                                       self.b.down_cast().vec())
-        self.solver = Amesos.Factory().Create(solver, problem)
+        #self.b = A.create_vec(dim=0)
+        #self.x = A.create_vec(dim=1)
+        #problem = Epetra.LinearProblem(A.down_cast().mat(),
+        #                               self.x.down_cast().vec(),
+        #                               self.b.down_cast().vec())
+        self.problem = Epetra.LinearProblem()
+        self.problem.SetOperator(A.down_cast().mat())
+        self.solver = Amesos.Factory().Create(solver, self.problem)
         err = self.solver.SymbolicFactorization()
-        if err > 1:
-            raise RuntimeError("Amesos " + solver + " symbolic factorization failed")
+        if err != 0:
+            raise RuntimeError("Amesos " + solver + " symbolic factorization failed, err=%d"%err)
         err = self.solver.NumericFactorization()
-        if err > 1:
-            raise RuntimeError("Amesos " + solver + " numeric factorization failed")
+        if err != 0:
+            raise RuntimeError("Amesos " + solver + " numeric factorization failed, err=%d"%err)
 
     def matvec(self, b):
         from dolfin import GenericVector
         if not isinstance(b, GenericVector):
             return NotImplemented()
-        if len(self.b) != len(b):
+        if self.A.size(0) != len(b):
             raise RuntimeError(
                 'incompatible dimensions for Amesos matvec, %d != %d'%(len(self.b),len(b)))
-        self.b[:] = b
-        self.solver.Solve()
-        return self.x
+        x = self.A.create_vec(dim=1)
+
+        self.problem.SetLHS(x.down_cast().vec())
+        self.problem.SetRHS(b.down_cast().vec())
+        err = self.solver.Solve()
+        self.problem.SetLHS(None)
+        self.problem.SetRHS(None)
+
+        if err != 0:
+            raise RuntimeError("Amesos solve failed, err=%d"%err)
+        return x
 
     def __str__(self):
         return '<%s solver for %s>'%(self.__class__.__name__, str(self.A))
