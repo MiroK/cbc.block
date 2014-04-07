ea47861655d4b54b1d5cce9b66be0bebab0779b2
Joachim B Haga <jobh@simula.no>
2014-04-07 15:51:14 +0200

    Avoid modifying the global PETSc options database (i.e., restore it after use)

--- block/algebraic/petsc/precond.py
+++ block/algebraic/petsc/precond.py
@@ -28,8 +28,26 @@ class precond(block_base):
         self.ml_prec.create()
         self.ml_prec.setType(prectype)
         self.ml_prec.setOperators(Ad, Ad, PETSc.Mat.Structure.SAME_PRECONDITIONER)
+
+        # Merge options from various sources into the options database
+        origOptions = PETSc.Options().getAll()
+        options = {}
+        options.update(_defaultOptions)
+        options.update(origOptions)
+        if parameters:
+            options.update(parameters)
+        for key,val in options.iteritems():
+            PETSc.Options().setValue(key, val)
+
+        # Create preconditioner based on the options database
         self.ml_prec.setFromOptions()
         self.ml_prec.setUp()
+
+        # Reset the options database
+        for key in options.iterkeys():
+            PETSc.Options().delValue(key)
+        for key,val in origOptions.iteritems():
+            PETSc.Options().setValue(key, val)
 
         info('constructed %s preconditioner in %.2f s'%(self.__class__.__name__, time()-T))
 
@@ -52,15 +70,24 @@ class precond(block_base):
         return '<%s prec of %s>'%(self.__class__.__name__, str(self.A))
 
 class ML(precond):
-    # Set some "safe" options, which can handle zero diagonal entries and makes
-    # the operator linear..
-    PETSc.Options().setValue('-mg_coarse_ksp_type', 'preonly')
-    PETSc.Options().setValue('-mg_coarse_pc_type', 'lu')
-    PETSc.Options().setValue('-mg_levels_ksp_type', 'richardson')
-    PETSc.Options().setValue('-mg_levels_pc_type', 'ilu')
     def __init__(self, A, parameters=None, pdes=1, nullspace=None):
         precond.__init__(self, A, PETSc.PC.Type.ML, parameters, pdes, nullspace)
 
 class ILU(precond):
     def __init__(self, A, parameters=None, pdes=1, nullspace=None):
         precond.__init__(self, A, PETSc.PC.Type.ILU, parameters, pdes, nullspace)
+
+
+# Set some "safe" options
+_defaultOptions = {
+    # Symmetry- and PD-preserving smoother
+    'mg_levels_ksp_type': 'chebyshev',
+    'mg_levels_pc_type':  'jacobi',
+    # Fixed number of iterations to preserve linearity
+    'mg_levels_ksp_max_it':               2,
+    'mg_levels_ksp_check_norm_iteration': 9999,
+    # Exact inverse on coarse grid
+    'mg_coarse_ksp_type': 'preonly',
+    'mg_coarse_pc_type':  'lu',
+    }
+
